7e77bf18a1e4aed626633b653c99dcbd1897f527
Jorge Cosgayon <jorge.cosgayon@gmail.com>
2021-10-06 11:32:15 +0800

    API logging

--- components/intro.js
+++ components/intro.js
@@ -4,24 +4,25 @@ import 'react-alice-carousel/lib/alice-carousel.css';
 
 const handleDragStart = (e) => e.preventDefault();
 
-// const items = [
-//     <img src="/images/splash-01.png" onDragStart={handleDragStart} role="presentation"/>,
-//     <img src="/images/factors.png" onDragStart={handleDragStart} role="presentation"/>,
-//     <img src="/images/finance.png" onDragStart={handleDragStart} role="presentation"/>,
-//     <img src="/images/investing.png" onDragStart={handleDragStart} role="presentation"/>,
-//     <img src="/images/revenue.png" onDragStart={handleDragStart} role="presentation"/>,
-//     <img src="/images/wallet.png" onDragStart={handleDragStart} role="presentation"/>,
-// ];
+const items = [
+    <img src="/images/splash-01.png" onDragStart={handleDragStart} role="presentation"/>,
+    <img src="/images/factors.png" onDragStart={handleDragStart} role="presentation"/>,
+    <img src="/images/finance.png" onDragStart={handleDragStart} role="presentation"/>,
+    <img src="/images/investing.png" onDragStart={handleDragStart} role="presentation"/>,
+    <img src="/images/revenue.png" onDragStart={handleDragStart} role="presentation"/>,
+    <img src="/images/wallet.png" onDragStart={handleDragStart} role="presentation"/>,
+];
 
 export default function Intro({carouselItems}) {
-    let items = []
-    for(let i = 0; i < carouselItems.length; i++){
-        items.push(
-            <a href={carouselItems[i].mainCarouselItem?.url}>
-                <img src={carouselItems[i].featuredImage?.node?.sourceUrl} onDragStart={handleDragStart} role="presentation" />
-            </a>
-        )
-    }
+    // let items = []
+    // for(let i = 0; i < carouselItems.length; i++){
+    //     items.push(
+    //         <a href={carouselItems[i].mainCarouselItem?.url}>
+    //             <img src={carouselItems[i].featuredImage?.node?.sourceUrl} onDragStart={handleDragStart} role="presentation" />
+    //         </a>
+    //     )
+    // }
+    console.log(carouselItems)
     return (
         <>
             <AliceCarousel mouseTracking items={items}/>
