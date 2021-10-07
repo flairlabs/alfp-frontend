53721769a4ed077f1a6501b1e6b34758a13cdbe2
Jorge Cosgayon <jorge.cosgayon@gmail.com>
2021-10-07 10:52:47 +0800

    update links

--- components/cover-image.js
+++ components/cover-image.js
@@ -18,9 +18,7 @@ export default function CoverImage({ title, coverImage, slug }) {
   return (
     <div className="sm:mx-0">
       {slug ? (
-        <Link href={`/posts/${slug}`}>
-          <a aria-label={title}>{image}</a>
-        </Link>
+          {image}
       ) : (
         image
       )}
