069033f7332980864ce09974e031ed163025cf8b
Jorge Cosgayon <jorge.cosgayon@gmail.com>
2021-10-07 11:56:10 +0800

    fixed bad link

--- components/cover-image.js
+++ components/cover-image.js
@@ -2,28 +2,23 @@ import cn from 'classnames'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default function CoverImage({ title, coverImage, slug }) {
-  const image = (
-    <Image
-      width={2000}
-      height={1000}
-      alt={`Cover Image for ${title}`}
-      src={coverImage?.sourceUrl}
-      className={cn('shadow-small', {
-        'hover:shadow-medium transition-shadow duration-200': slug,
-      })}
-    />
-      // <img src={coverImage?.sourceUrl} />
-  )
-  return (
-    <div className="sm:mx-0">
-      {slug ? (
-        <Link href={`/posts/${slug}`}>
-          <a aria-label={title}>{image}</a>
-        </Link>
-      ) : (
-        image
-      )}
-    </div>
-  )
+export default function CoverImage({title, coverImage, slug}) {
+    const src = coverImage?.sourceUrl
+    return (
+        <div className="sm:mx-0">
+            {src ? (
+                <Image
+                    width={2000}
+                    height={1000}
+                    alt={`Cover Image for ${title}`}
+                    src={coverImage?.sourceUrl}
+                    className={cn('shadow-small', {
+                        'hover:shadow-medium transition-shadow duration-200': slug,
+                    })}
+                />
+            ) : (
+                <span>no image</span>
+            )}
+        </div>
+    )
 }
