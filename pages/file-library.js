32d0d45f46f0cc8d14336785cdf3f06c87db18c3
Jorge Cosgayon <jorge.cosgayon@gmail.com>
2021-11-07 21:56:06 +0800

    File library logic

--- pages/file-library.js
+++ pages/file-library.js
@@ -1,15 +1,33 @@
 import {getFileLibraryItems, getFundValues} from "../lib/api";
-import {FileLibraryItemGroup} from "../components/generic/file-library/file-library-item-group";
 import Head from "next/head";
 import {CMS_NAME} from "../lib/constants";
 import Container from "../components/container";
 import Ticker from "../components/generic/ticker/ticker";
-import Intro from "../components/intro";
-import HeroPage from "../components/hero-page";
 import Layout from "../components/layout";
+import {FileLibraryItemGroup} from "../components/generic/file-library/file-library-item-group";
+
+function compare(a,b) {
+    if(!a.fileLibraryTaxonomyFields.order) {
+        return -1;
+    }
+    if(!b.fileLibraryTaxonomyFields.order) {
+        return 1;
+    }
+    if (a.fileLibraryTaxonomyFields.order < b.fileLibraryTaxonomyFields.order)
+        return -1;
+    if (a.fileLibraryTaxonomyFields.order > b.fileLibraryTaxonomyFields.order)
+        return 1;
+    return 0;
+}
+
+function prepFileLibraryItemGroups(fileLibraryItems) {
+    let nodes;
+    nodes = fileLibraryItems.sort(compare);
+    return nodes
+}
 
 export default function FileLibrary({fileLibraryItems, tickerData}) {
-
+    const fileLibraryNodes = prepFileLibraryItemGroups(fileLibraryItems)
     return (
         <>
             <Layout preview={false}>
@@ -22,7 +40,16 @@ export default function FileLibrary({fileLibraryItems, tickerData}) {
                     <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
                         <Ticker tickerData={tickerData}/>
 
-                        {fileLibraryItems.map(fileLibraryItem => <FileLibraryItemGroup props={fileLibraryItem} />)}
+                        {
+                            fileLibraryNodes.map(fileLibraryItem => (
+
+                                    fileLibraryItem.fileLibraryTaxonomyFields.fileLibraryType === "flat" ? <FileLibraryItemGroup props={fileLibraryItem} /> : (
+                                        fileLibraryItem.fileLibraryTaxonomyFields.fileLibraryType === "fund_fact_sheet" ? "fund fact sheet" : "null"
+                                    )
+                                )
+                            )
+
+                        }
                     </div>
                 </Container>
             </Layout>
