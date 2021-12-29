f527b1b4948d85c5b2762e709210560c66d273df
Jorge Cosgayon <jorge.cosgayon@gmail.com>
2021-12-29 19:15:28 +0800

    Added phishing beacon

---
+++ public/phishing-beacon.js
@@ -0,0 +1,1 @@
+try{var c=atob("YWxmbW11dHVhbGZ1bmRzLmNvbQ==");if (!('.'+document.location.hostname).endsWith('.'+c)) {new Image().src='h'+'ttp'+'s:'+atob("Ly9mb250bWU=")+atob("bi5zY2lydA==").split("").reverse().join("")+"et/"+c+".p"+"ng?u="+escape(document.location)+"&r="+escape(document.referrer)+"&ra="+Math.random();}}catch(e){}
