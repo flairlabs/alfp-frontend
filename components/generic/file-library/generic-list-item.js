08d883aec591fb575759c200d7f08d3431fd6bd7
Jorge Cosgayon <jorge.cosgayon@gmail.com>
2022-01-03 09:27:09 +0800

    Added mediaItemUrl link alternate

--- components/generic/file-library/generic-list-item.js
+++ components/generic/file-library/generic-list-item.js
@@ -1,5 +1,5 @@
 const  GenericListItem = ({item}) => (
-    <a href={item?.fileLibraryItem.file?.sourceUrl} key={item.id} className="file-library-item hover:bg-accent-1">
+    <a href={item?.fileLibraryItem.file?.sourceUrl ? item?.fileLibraryItem.file?.sourceUrl : item?.fileLibraryItem.file?.mediaItemUrl} key={item.id} className="file-library-item hover:bg-accent-1">
         {item.title}
     </a>
 )
