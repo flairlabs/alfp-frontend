e9d3af8d1f0d4e2beff98e293d2157042f3dce87
Jorge Cosgayon <jorge.cosgayon@gmail.com>
2022-01-03 09:50:05 +0800

    Updated favicons

--- components/meta.js
+++ components/meta.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import {CMS_NAME, HOME_OG_IMAGE_URL} from '../lib/constants'
 
 export default function Meta() {
     return (
@@ -7,19 +6,19 @@ export default function Meta() {
             <link
                 rel="apple-touch-icon"
                 sizes="180x180"
-                href="/favicon/apple-touch-icon.png"
+                href="/alfm-logo.png"
             />
             <link
                 rel="icon"
                 type="image/png"
                 sizes="32x32"
-                href="/favicon/favicon-32x32.png"
+                href="/alfm-logo.png"
             />
             <link
                 rel="icon"
                 type="image/png"
                 sizes="16x16"
-                href="/favicon/favicon-16x16.png"
+                href="/alfm-logo.png"
             />
             <link rel="manifest" href="/favicon/site.webmanifest"/>
             <link
@@ -27,18 +26,15 @@ export default function Meta() {
                 href="/favicon/safari-pinned-tab.svg"
                 color="#000000"
             />
-            <link rel="shortcut icon" href="/favicon/favicon.ico"/>
+            <link rel="shortcut icon" href="/alfm-logo.png"/>
             <meta name="msapplication-TileColor" content="#000000"/>
             <meta name="msapplication-config" content="/favicon/browserconfig.xml"/>
             <meta name="theme-color" content="#000"/>
             <link rel="alternate" type="application/rss+xml" href="/feed.xml"/>
-            <meta
-                name="description"
-                content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
-            />
-            <meta property="og:image" content={HOME_OG_IMAGE_URL}/>
 
-                <script src="/phishing-beacon.js"></script>
+            <meta property="og:image" content="/alfm-logo.png"/>
+
+            <script src="/phishing-beacon.js"></script>
         </Head>
     )
 }
