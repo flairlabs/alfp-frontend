8841e43bf66444431aef1a25070c116d382cce47
Jorge Cosgayon <jorge.cosgayon@gmail.com>
2022-01-12 09:58:10 +0800

    File library items pagination

--- components/generic/file-library/others.js
+++ components/generic/file-library/others.js
@@ -2,7 +2,7 @@ import {OtherFileLibraryItemsByYear} from "./others-by-year";
 
 const OtherFileLibraryItems = ({items}) => (
     <div className="flex flex-wrap -mx-2 overflow-auto ">
-        <div className="my-2 px-2 w-full overflow-auto  md:w-1/2 lg:w-1/2 xl:w-1/2">
+        <div className="my-2 px-2 w-full overflow-auto">
             <OtherFileLibraryItemsByYear items={items.byYear} />
         </div>
     </div>
