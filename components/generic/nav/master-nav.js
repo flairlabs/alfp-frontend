583c0920ee61f6e9794053dd14fc57b46b6c973d
Jorge Cosgayon <jorge.cosgayon@gmail.com>
2022-01-24 16:14:51 +0800

    Removed padding on dropdown

--- components/generic/nav/master-nav.js
+++ components/generic/nav/master-nav.js
@@ -180,7 +180,7 @@ export default function MasterNavbar({theme}) {
                                                             <Menu.Button
                                                                 key={"menuItemButtom-" + item.name}
                                                                 aria-current={item.current ? 'page' : undefined}
-                                                                className="inline-flex justify-center w-full px-3 py-2 font-medium">
+                                                                className="inline-flex justify-center w-full px-3 font-medium">
                                                                 {item.name}
                                                                 <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5"
                                                                                  aria-hidden="true"/>
