fd01a50926e18eda1d8be2b2bf66605877166836
Jorge Cosgayon <jorge.cosgayon@gmail.com>
2022-03-10 14:43:25 +0800

    Removed redundant calls

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,16 +1,10 @@
 import {useRouter} from 'next/router'
 import ErrorPage from 'next/error'
-import Container from '../../components/container'
 import PostBody from '../../components/post-body'
 import MoreStories from '../../components/more-stories'
-import Header from '../../components/header'
 import PostHeader from '../../components/post-header'
 import SectionSeparator from '../../components/section-separator'
-import Layout from '../../components/layout'
 import {getAllPostsWithSlug, getFundValues, getPostAndMorePosts} from '../../lib/api'
-import PostTitle from '../../components/post-title'
-import Head from 'next/head'
-import {CMS_NAME} from '../../lib/constants'
 import Tags from '../../components/tags'
 import PageLayout from "../../layouts/PageLayout";
 
@@ -43,19 +37,6 @@ export default function Post({post, posts, preview, tickerData}) {
 
     )
 }
-
-// export async function getStaticProps({ params, preview = false, previewData }) {
-//   const data = await getPostAndMorePosts(params.slug, preview, previewData)
-//   const tickerData = await getFundValues(2)
-//   return {
-//     props: {
-//       preview,
-//       post: data.post,
-//       posts: data.posts,
-//       tickerData
-//     },
-//   }
-// }
 
 export async function getServerSideProps( {params, preview = false, previewData }) {
     const allPosts = await getAllPostsWithSlug()
