d4e1e9cf1a0eadfaeb25a8bfe227cafd97ae48da
Jorge Cosgayon <jorge.cosgayon@gmail.com>
2022-03-24 10:13:43 +0800

    Removed email link

--- components/footer.js
+++ components/footer.js
@@ -56,7 +56,7 @@ export default function Footer() {
                                 <rect x="3" y="5" width="18" height="14" rx="2"/>
                                 <polyline points="3 7 12 13 21 7"/>
                             </svg>
-                            <a href="mailto:bpi_investment@bpi.com.ph">bpi_investment@bpi.com.ph</a>
+                            bpi_investment@bpi.com.ph
                         </p>
                     </div>
                     <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
