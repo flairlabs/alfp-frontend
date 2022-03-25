a5bef74e50b457feb4f3b005f467e30b3264afa1
Jorge Cosgayon <jorge.cosgayon@gmail.com>
2022-03-25 11:40:37 +0800

    Investigating weird behavior in getFundValues call

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,6 @@ export default function Index({allPosts: {edges}, preview, frontPages, allMainCa
     return (
         <>
             <PageLayout title="Home" preview={false} tickerData={tickerData} relatedSites={relatedSites}>
-
                 <Intro carouselItems={allMainCarouselItems}/>
 
                 {heroPage && (
@@ -162,6 +161,11 @@ export default function Index({allPosts: {edges}, preview, frontPages, allMainCa
 
                     </div>
                 </div>
+                {tickerData.map((item) => (
+                    <>{item.title}</>
+                ))}
+
+
             </PageLayout>
         </>
     )
